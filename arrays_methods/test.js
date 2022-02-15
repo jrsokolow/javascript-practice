a096043f7aad99a404c667ca6fc7c1a26775ad94
Jakub <jrsokolow@gmail.com>
2022-02-15 14:09:33 +0100

    add console.assert

--- arrays_methods/test.js
+++ arrays_methods/test.js
@@ -5,16 +5,16 @@ const calculator = new AdvancedCalculator();
 const input = [1,2,3,4];
 
 const sumResult = calculator.sum(input);
-console.log('sum result: ' + sumResult);
+console.assert(10 === sumResult, "For each example test failed");
 
 const sumViaReduceResult = calculator.sumViaReduce(input);
-console.log('sum via reduce:' + sumViaReduceResult);
+console.assert(10 === sumViaReduceResult), "Reduce example test failed";
 
 const squeredOddResult = calculator.squerOdd(input);
-console.log('odd squered:' + squeredOddResult);
+console.assert(1,9 === squeredOddResult, "Filter example test failed");
 
 const instancesInCentemeters = [100, 200, 300];
 const instancesInMeters = convertToMeters(instancesInCentemeters);
-console.log(1 === instancesInMeters[0].distanceInMeters);
-console.log(2 === instancesInMeters[1].distanceInMeters);
-console.log(3 === instancesInMeters[2].distanceInMeters);
+console.assert(1 === instancesInMeters[0].distanceInMeters, "Filter map test failed for 1m");
+console.assert(2 === instancesInMeters[1].distanceInMeters, "Filter map test failed for 2m");
+console.assert(3 === instancesInMeters[2].distanceInMeters, "Filter map test failed for 3m");
